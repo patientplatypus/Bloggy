9716a20485891534e079f133a62ede66ec324c1a
Peter Weyand <pweyand@gmail.com>
2017-09-23 22:01:03 -0500

    add gulp and semantic theme builder - use default theme for now

--- frontend/blog/src/index.js
+++ frontend/blog/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import "semantic-ui-css/semantic.css";
+// import "semantic-ui-css/semantic.css";
+// import './semantic-ui/semantic-ui.less'
+// import './semantic/semantic';
+// import './semantic/semantic.less';
+import './semantic/dist/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
